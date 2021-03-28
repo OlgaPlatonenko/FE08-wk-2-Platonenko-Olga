6523bf5a8579b7b94ffb422f13d5ddcd7ad8717b
OlgaPlatonenko <platonenkoolga@gmail.com>
2021-03-28 18:26:18 +0300

    without burger

--- js/script.js
+++ js/script.js
@@ -3,31 +3,7 @@ $(document).ready(function(){
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }]     
+    slidesToShow: 1,
+    adaptiveHeight: true
   });
 });
